bf74308790413308f0d0e5f3be899b9845271698
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2020-11-01 19:50:05 +0000

    Drop python2 support and improve codestyle
    - Use pep8 codestyle
    - Use logging instead of print

--- tplink_smartplug.py
+++ tplink_smartplug.py
@@ -19,133 +19,138 @@
 # limitations under the License.
 #
 
-import sys
-import socket
 import argparse
+import logging
+import socket
+import sys
 from struct import pack
+log = logging.getLogger(__name__)
+datalog = logging.getLogger("datalog")
 
-version = 0.3
+version = 0.31
 
-# Check if hostname is valid
-def validHostname(hostname):
-	try:
-		socket.gethostbyname(hostname)
-	except socket.error:
-		parser.error("Invalid hostname.")
-	return hostname
 
-# Check if port is valid
-def validPort(port):
-        try:
-            port = int(port)
-        except ValueError:
-            parser.error("Invalid port number.")
-
-        if ((port <= 1024) or (port >65535)) :
-            parser.error("Invalid port number.")
-
-        return port
+def configure_logging():
+    log.addHandler(logging.StreamHandler(sys.stderr))
+    log.setLevel(logging.INFO)
+    log.propagate = False
+    datalog.addHandler(logging.StreamHandler(sys.stdout))
+    datalog.propagate = False
+    datalog.setLevel(logging.INFO)
 
 
 # Predefined Smart Plug Commands
 # For a full list of commands, consult tplink_commands.txt
-commands = {		'info'     : '{"system":{"get_sysinfo":{}}}',
-			'on'       : '{"system":{"set_relay_state":{"state":1}}}',
-			'off'      : '{"system":{"set_relay_state":{"state":0}}}',
-	    		'ledoff'   : '{"system":{"set_led_off":{"off":1}}}',
-			'ledon'    : '{"system":{"set_led_off":{"off":0}}}',
-			'cloudinfo': '{"cnCloud":{"get_info":{}}}',
-			'wlanscan' : '{"netif":{"get_scaninfo":{"refresh":0}}}',
-			'time'     : '{"time":{"get_time":{}}}',
-			'schedule' : '{"schedule":{"get_rules":{}}}',
-			'countdown': '{"count_down":{"get_rules":{}}}',
-			'antitheft': '{"anti_theft":{"get_rules":{}}}',
-			'reboot'   : '{"system":{"reboot":{"delay":1}}}',
-			'reset'    : '{"system":{"reset":{"delay":1}}}',
-			'energy'   : '{"emeter":{"get_realtime":{}}}'
+commands = {
+    'info':      '{"system":{"get_sysinfo":{}}}',
+    'on':        '{"system":{"set_relay_state":{"state":1}}}',
+    'off':       '{"system":{"set_relay_state":{"state":0}}}',
+    'ledoff':    '{"system":{"set_led_off":{"off":1}}}',
+    'ledon':     '{"system":{"set_led_off":{"off":0}}}',
+    'cloudinfo': '{"cnCloud":{"get_info":{}}}',
+    'wlanscan':  '{"netif":{"get_scaninfo":{"refresh":0}}}',
+    'time':      '{"time":{"get_time":{}}}',
+    'schedule':  '{"schedule":{"get_rules":{}}}',
+    'countdown': '{"count_down":{"get_rules":{}}}',
+    'antitheft': '{"anti_theft":{"get_rules":{}}}',
+    'reboot':    '{"system":{"reboot":{"delay":1}}}',
+    'reset':     '{"system":{"reset":{"delay":1}}}',
+    'energy':    '{"emeter":{"get_realtime":{}}}'
 }
+
 
 # Encryption and Decryption of TP-Link Smart Home Protocol
 # XOR Autokey Cipher with starting key = 171
-# Python 3.x Version
-if sys.version_info[0] > 2:
-	def encrypt(string):
-		key = 171
-		result = pack('>I', len(string))
-		for i in string:
-			a = key ^ ord(i)
-			key = a
-			result += bytes([a])
-		return result
-
-	def decrypt(string):
-		key = 171
-		result = ""
-		for i in string:
-			a = key ^ i
-			key = i
-			result += chr(a)
-		return result
-
-# Python 2.x Version
-else:
-	def encrypt(string):
-		key = 171
-		result = pack('>I', len(string))
-		for i in string:
-			a = key ^ ord(i)
-			key = a
-			result += chr(a)
-		return result
-
-	def decrypt(string):
-		key = 171
-		result = ""
-		for i in string:
-			a = key ^ ord(i)
-			key = ord(i)
-			result += chr(a)
-		return result
-
-# Parse commandline arguments
-parser = argparse.ArgumentParser(description="TP-Link Wi-Fi Smart Plug Client v" + str(version))
-parser.add_argument("-t", "--target", metavar="<hostname>", required=True, help="Target hostname or IP address", type=validHostname)
-parser.add_argument("-p", "--port", metavar="<port>", default=9999, required=False, help="Target port", type=validPort)
-parser.add_argument("-q", "--quiet", dest='quiet', action='store_true', help="Only show result")
-parser.add_argument("--timeout", default=10, required=False, help="Timeout to establish connection")
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-c", "--command", metavar="<command>", help="Preset command to send. Choices are: "+", ".join(commands), choices=commands)
-group.add_argument("-j", "--json", metavar="<JSON string>", help="Full JSON string of command to send")
-args = parser.parse_args()
+def encrypt(string):
+    if not isinstance(string, bytes):
+        string = string.encode()
+    key = 171
+    result = bytearray()
+    for i in string:
+        a = key ^ i
+        key = a
+        result.append(a)
+    msg = pack('>I', len(string)) + bytes(result)
+    return msg
 
 
-# Set target IP, port and command to send
-ip = args.target
-port = args.port
-if args.command is None:
-	cmd = args.json
-else:
-	cmd = commands[args.command]
+def decrypt(string):
+    key = 171
+    result = bytearray()
+    for i in string:
+        a = key ^ i
+        key = i
+        result.append(a)
+    return bytes(result)
 
 
+def parse_args():
+    # Parse commandline arguments
+    parser = argparse.ArgumentParser(
+        description="TP-Link Wi-Fi Smart Plug Client v" + str(version))
 
-# Send command and receive reply
-try:
-	sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock_tcp.settimeout(int(args.timeout))
-	sock_tcp.connect((ip, port))
-	sock_tcp.settimeout(None)
-	sock_tcp.send(encrypt(cmd))
-	data = sock_tcp.recv(2048)
-	sock_tcp.close()
+    # Check if hostname is valid
+    def validHostname(hostname):
+        try:
+            socket.gethostbyname(hostname)
+        except socket.error:
+            parser.error("Invalid hostname.")
+        return hostname
 
-	decrypted = decrypt(data[4:])
+    # Check if port is valid
+    def validPort(port):
+        if not port.isdigit() or int(port) <= 1024 or int(port) > 65535:
+            parser.error("Invalid port number.")
+        return int(port)
 
-	if args.quiet:
-		print(decrypted)
-	else:
-		print("Sent:     ", cmd)
-		print("Received: ", decrypted)
+    parser.add_argument("-t", "--target", metavar="<hostname>", required=True,
+                        help="Target hostname or IP address",
+                        type=validHostname)
+    parser.add_argument("-p", "--port", metavar="<port>", default=9999,
+                        required=False, help="Target port", type=validPort)
+    parser.add_argument("-q", "--quiet", dest='quiet', action='store_true',
+                        help="Only show result")
+    parser.add_argument("--timeout", default=10, required=False,
+                        help="Timeout to establish connection")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-c", "--command", metavar="<command>",
+                       help="Preset command to send. Choices are: "
+                             + ", ".join(commands), choices=commands)
+    group.add_argument("-j", "--json", metavar="<JSON string>",
+                       help="Full JSON string of command to send")
 
-except socket.error:
-	quit("Could not connect to host " + ip + ":" + str(port))
+    return parser.parse_args()
+
+
+def main():
+    # Set target IP, port and command to send
+    configure_logging()
+    args = parse_args()
+    if args.quiet:
+        log.disabled = True
+    if args.command is None:
+        cmd = args.json
+    else:
+        cmd = commands[args.command]
+
+    # Send command and receive reply
+    try:
+        sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_tcp.settimeout(int(args.timeout))
+        sock_tcp.connect((args.target, args.port))
+        sock_tcp.settimeout(None)
+        sock_tcp.sendall(encrypt(cmd))
+        # we want to send user messages to standard error so that the data can
+        # be easily piped to another command like jq
+        log.info(f"Sent:     {cmd}\nReceiving... ")
+        data = sock_tcp.recv(2048)
+        sock_tcp.close()
+
+        decrypted = decrypt(data[4:]).decode()
+        datalog.info(decrypted)
+    except socket.error:
+        quit(f"Could not connect to host {args.target}:{args.port}")
+
+
+if __name__ == "__main__":
+    main()
